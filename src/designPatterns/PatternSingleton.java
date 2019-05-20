2c95ce0d2f0fc9addd5b0c845c455dbefe0b1a96
Renato Lira Camarao <renato.camarao@itbam.org.br>
2019-05-20 19:03:03 -0400

    design pattern singleton

---
+++ src/designPatterns/PatternSingleton.java
@@ -0,0 +1,18 @@
+package designPatterns;
+
+public class PatternSingleton {
+
+    private static PatternSingleton patternSingleton = new PatternSingleton();
+
+    private int x = 0;
+
+    private PatternSingleton(){ }
+
+    public static PatternSingleton getInstance(){
+        return patternSingleton;
+    }
+
+    public int getX(){
+        return x++;
+    }
+}
