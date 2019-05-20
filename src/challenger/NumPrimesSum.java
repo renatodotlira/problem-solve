c71df90122efbbb5564afc9d514a08b01537d1aa
Renato Lira Camarao <renato.camarao@itbam.org.br>
2019-05-20 14:34:47 -0400

    Find the sum of all the primes below two million

---
+++ src/challenger/NumPrimesSum.java
@@ -0,0 +1,40 @@
+package challenger;
+
+//The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+//Find the sum of all the primes below two million.
+
+import java.math.BigInteger;
+
+public class NumPrimesSum {
+
+    public static void main(String...args){
+        Prime p = new Prime();
+        System.out.println(p.printPrimes(2000000));
+    }
+}
+
+class Prime{
+    private BigInteger total = BigInteger.valueOf(2);
+
+    BigInteger printPrimes(int quantity){
+        int x = 3;
+        for (; x <= quantity;){
+            if(isPrime(x)) {
+                total = total.add(BigInteger.valueOf(x));
+            }
+            x = x + 2;
+        }
+        return total;
+    }
+
+    private boolean isPrime(int x){
+        int range = (int) Math.sqrt(x);
+        for (int i = 3; i <= range ;){
+            if(x % i ==0)
+                return false;
+            i = i + 2;
+            range = x / i;
+        }
+        return true;
+    }
+}
